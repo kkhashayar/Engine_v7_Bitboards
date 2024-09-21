a475e4fbc49a3e4017ef866cc962b6ecb759a704
kkhashayar <frankdezz@gmail.com>
2024-09-21 17:23:59 +0200

    added external resources, connect to stuck fish to verify perft acuracy

--- Bb_Ui/Program.cs
+++ Bb_Ui/Program.cs
@@ -1,19 +1,64 @@
 ﻿using Bb_Engine;
+using Bb_Engine.ExternalResources;
 
 string fen = "3k4/8/8/8/8/8/8/3K4 w - - 0 1";
 Fen.Read(fen);
 
-
-Perft.TestPerft(fen, 2);
-Console.ReadKey();  
-
-
+RunPerftWIthVerification(fen, 2);
 
 PrintBoard(Boards.GetBoards());
 
 PrintBoardSquareNumbers();
 
-//////////////////////////////////////////////////////////////// Functions //////////////////////////////////////////////////////////////// 
+//////////////////////////////////////////////////////////////// Functions ////////////////////////////////////////////////////////////////
+void RunPerftWIthVerification(string fen, int perftDepth)
+{
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.WriteLine("******* Engine 6 *******  \n");
+    Console.WriteLine($"Perft test in depth: {perftDepth} on: \n");
+    Console.WriteLine($"{fen} \n");
+    Perft.TestPerft(fen, perftDepth);
+    Console.WriteLine();
+    Console.Beep(2000, 50);
+    Console.WriteLine("Press 'V' to verify with Stockfish or any key to exit \n");
+    
+    char input = Console.ReadKey().KeyChar;
+    if (input == 'v' || input == 'V')
+    {
+        VerifyWithStockfish(fen, perftDepth);
+        Console.WriteLine("Press any key to continue\n");
+        Console.ReadKey();    
+    }
+
+    Environment.Exit(0);    
+}
+
+
+void VerifyWithStockfish(string fen, int depth)
+{
+    string stockfishPath = "\"D:\\DATA\\stockfish_15.1_win_x64_avx2\\stockfish-windows-2022-x86-64-avx2.exe\"";
+    StockfishIntegration stockfish = new StockfishIntegration(stockfishPath);
+    stockfish.StartStockfish();
+
+    // Send the FEN to Stockfish
+    stockfish.SendCommand($"position fen {fen}");
+    stockfish.SendCommand($"go perft {depth}");
+
+    // Read the output
+    string output;
+
+    while ((output = stockfish.ReadOutput()) != null)
+    {
+
+        Console.WriteLine(output);
+        if (output.StartsWith("Stockfish result:  ")) break;
+    }
+
+}
+
+
+
+
 void PrintBoard(List<ulong> boards)
 {
     for (int rank = 7; rank >= 0; rank--) // start from the 8th rank and move down to the 1st
